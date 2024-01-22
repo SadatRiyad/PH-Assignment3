908b8b417dbb3f55cfcc80374267fd1a7f53e5d2
SadatRiyad <sadatriyad@students.diu.ac>
2024-01-23 02:41:31 +0600

    navbar added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+// for dark mode daisyui
+module.exports = {
+  //...
+  daisyui: {
+    themes: ["light", "dark"],
+  },
+};
